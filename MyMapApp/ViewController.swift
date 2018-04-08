8b03a84cad57984d1ccad2d8af2cde9dfdf412f3
Zhanpeng Liang <hectorilles@gmail.com>
2018-04-07 17:51:00 -0700

    map routing

--- MyMapApp/ViewController.swift
+++ MyMapApp/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate,
 
     @IBOutlet weak var destSearchBar: UISearchBar!
     @IBOutlet weak var myMapView: MKMapView!
+    @IBOutlet weak var directionInstruction: UILabel!
     
     let locationManager = CLLocationManager()
     
@@ -23,7 +24,11 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate,
     var searchedAnnotations = [MKAnnotation]()
     var tappedAnnotation: MKAnnotation?
     
+    // Direction related
     var destination: MKMapItem?
+    var markedOverlays = [MKOverlay]()
+    var directionSteps: [MKRouteStep]?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,7 +62,7 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate,
         let region = MKCoordinateRegion(center: currentLocationCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
 //        myMapView.setCenter(currentLocationCoordinate, animated: true)
         myMapView.setRegion(region, animated: true)
-        locationManager.stopUpdatingLocation() // temperary
+//        locationManager.stopUpdatingLocation() // temperary
     }
     
     // Mark: search for destination
@@ -137,11 +142,13 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate,
         if gestureRecognizer.state == .began {
             let location = gestureRecognizer.location(in: myMapView)
             let tappedCoordinate = myMapView.convert(location, toCoordinateFrom: myMapView)
+            let lat = tappedCoordinate.latitude
+            let lon = tappedCoordinate.longitude
             
             let annotation = MKPointAnnotation()
             annotation.coordinate = tappedCoordinate
             annotation.title = "selected location"
-            annotation.subtitle = "..."
+            annotation.subtitle = "lat: \(lat), lon: \(lon)"
             tappedAnnotation = annotation
             myMapView.addAnnotation(annotation)
         }
@@ -149,18 +156,24 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate,
     
     // Mark: on tap behavior
     @IBAction func handleTapInMap(_ gestureRecognizer: UITapGestureRecognizer) {
+        removeTappedAnnot()
+    }
+    
+    func removeTappedAnnot() {
         if tappedAnnotation != nil {
             myMapView.removeAnnotation(tappedAnnotation!)
             tappedAnnotation = nil
         }
     }
-    
-    // TEMP: user location functionality beyond initial retrieval omitted
 }
 
 extension ViewController {
+    
+    
     // Mark: direction
     func getDirection(to dest: MKMapItem?) {
+        cleanupForNewRoute()
+        
         if dest == nil {
             print("geting directions: no destination selected")
         }
@@ -180,6 +193,7 @@ extension ViewController {
                 print("geting directions Error: \(error)")
                 return
             }
+                    
             self.drawSingleRoute(response!.routes.first) // check later
         }
     }
@@ -187,15 +201,20 @@ extension ViewController {
     // MARK: customize overlay style
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         if overlay is MKPolyline {
-            let renderer = MKPolylineRenderer()
-            renderer.strokeColor = UIColor.cyan
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.blue
             renderer.lineWidth = 5
+            return renderer
+        } else if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.yellow
+            renderer.alpha = 0.5
             return renderer
         } else {
             return MKOverlayRenderer()
         }
     }
-    
+
     // MARK: draw direction
     func drawSingleRoute(_ route: MKRoute?) {
         if route == nil {
@@ -204,7 +223,70 @@ extension ViewController {
         }
         print("drawSingleRoute:", route!)
         
-        // test
-        myMapView.addOverlays([route!.polyline], level: .aboveLabels)
+        let polyline = route!.polyline
+        markedOverlays.append(polyline)
+        myMapView.add(polyline, level: .aboveLabels)
+        
+        
+        let steps = route!.steps
+        self.directionSteps = steps
+        
+        print("drawSingleRoute: size of steps \(steps.count)")
+        
+        // do not clear monitoring: mind 20 mintors are allowed at the same time
+        for (i, step) in steps.enumerated() {
+            print(step.polyline)
+            let center = step.polyline.coordinate
+            let circle = MKCircle(center: center, radius: 20)
+            
+            markedOverlays.append(circle)
+            myMapView.add(circle, level: .aboveLabels)
+            
+            // monitoring for the region
+            let region = CLCircularRegion(center: center, radius: 20, identifier: String(i))
+            self.locationManager.startMonitoring(for: region)
+        }
+    }
+    
+    // MARK: handle region entering events
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered Region: \(region)")
+        
+        let identifier = region.identifier
+        
+        guard let which = Int(identifier) else {
+            print("not recognized region identifier \(identifier)")
+            return
+        }
+        
+        var instruction = ""
+        if which == directionSteps!.count - 1 {
+            instruction = "you have arrived"
+        } else {
+            let steps = self.directionSteps!
+            instruction = "move \(steps[which+1].distance)m towards the next hop"
+        }
+        self.directionInstruction.text = instruction
+        
+        // MARK: consider reducing monitors
+        
+        
+    }
+    
+    func cleanupForNewRoute() {
+        clearMarkedOverlays()
+        clearRegionMonitoring()
+        self.directionSteps = nil
+    }
+    
+    func clearMarkedOverlays() {
+        myMapView.removeOverlays(markedOverlays)
+        markedOverlays.removeAll()
+    }
+    
+    func clearRegionMonitoring() {
+        locationManager.monitoredRegions.forEach { (region) in
+            locationManager.stopMonitoring(for: region)
+        }
     }
 }
